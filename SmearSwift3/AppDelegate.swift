b6c4ce1093b440a1f67a61a34c23ccd2452b7f39
Adam Jensen <acjensen@gmail.com>
2017-06-07 17:37:31 -0700

    Initial Commit

---
+++ SmearSwift3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SmearSwift3
+//
+//  Created by Adam Jensen on 6/7/17.
+//  Copyright © 2017 Adam Jensen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SmearSwift3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SmearSwift3
+//
+//  Created by Adam Jensen on 6/7/17.
+//  Copyright © 2017 Adam Jensen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
